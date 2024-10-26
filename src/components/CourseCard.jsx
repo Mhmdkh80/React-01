57e9484e8db7421fa51d089d4cd1d69358461466
mhmdkh80 <mohamadkh.tech@gamil.com>
2024-10-26 16:55:13 +0330

    render

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,36 @@
+function CourseCard() {
+  return (
+    <div className="course-item">
+      <div className="course-item__img">
+        <img src="/images/img1.jpg" alt="img-1" />
+      </div>
+      <div className="course-item__detail">
+        <div className="course-item__body">
+          <div>
+            <p className="title">React course</p>
+            <p className="desc">The Ultimate React and Redux</p>
+          </div>
+          <span className="rate">4</span>
+        </div>
+        <div className="course-item__footer">
+          <div className="tags">
+            <span className="badge badge--secondary">languages</span>
+            <span className="badge badge--secondary">languages</span>
+          </div>
+          <div className="caption">
+            <div className="date">
+              {new Date().toLocaleDateString("en-US", {
+                month: "short",
+                year: "numeric",
+                day: "numeric",
+              })}
+            </div>
+            <span className="badge badge--primary">completed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CourseCard;
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,3 +1,5 @@
+import CourseCard from "./CourseCard";
+
 const courses = [
   {
     id: 1,
@@ -36,38 +38,13 @@ const courses = [
 function CourseList() {
   return (
     <div className="course-list">
-      <div className="course-item">
-        <div className="course-item__img">
-          <img src="/images/img1.jpg" alt="img-1" />
-        </div>
-        <div className="course-item__detail">
-          <div className="course-item__body">
-            <div>
-              <p className="title">React course</p>
-              <p className="desc">The Ultimate React and Redux</p>
-            </div>
-            <span className="rate">4</span>
-          </div>
-          <div className="course-item__footer">
-            <div className="tags">
-              <span className="badge badge--secondary">languages</span>
-              <span className="badge badge--secondary">languages</span>
-            </div>
-            <div className="caption">
-              <div className="date">
-                {new Date().toLocaleDateString("en-US", {
-                  month: "short",
-                  year: "numeric",
-                  day: "numeric",
-                })}
-              </div>
-              <span className="badge badge--primary">completed</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      {courses.map((course) => {
+        return <CourseCard key={course.id} />;
+      })}
     </div>
   );
 }
 
 export default CourseList;
+
+
